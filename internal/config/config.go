62a7bbcd94f0cc3f19879f7e78aafb0eed43203e
Tea Nguyen <tung.nguyen3@tyme.com>
2026-05-03 16:24:49 +0700

    feat(config): add resiliency configuration (timeout, retry, deadline, breaker threshold)

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
 type Config struct {
@@ -10,6 +12,11 @@ type Config struct {
 	DatabaseURL    string
 	SalesMockURL   string
 	ServiceMockURL string
+
+	UpstreamTimeout  time.Duration
+	RetryBaseDelay   time.Duration
+	RequestDeadline  time.Duration
+	BreakerThreshold uint32
 }
 
 func Load() (*Config, error) {
@@ -33,10 +40,43 @@ func Load() (*Config, error) {
 		serviceURL = "http://localhost:9002"
 	}
 
+	upstreamTimeout := parseDuration("UPSTREAM_TIMEOUT_MS", 800)
+	retryBaseDelay := parseDuration("RETRY_BASE_DELAY_MS", 50)
+	requestDeadline := parseDuration("REQUEST_DEADLINE_MS", 1500)
+	breakerThreshold := parseUint32("BREAKER_THRESHOLD", 5)
+
 	return &Config{
-		Port:           port,
-		DatabaseURL:    dbURL,
-		SalesMockURL:   salesURL,
-		ServiceMockURL: serviceURL,
+		Port:             port,
+		DatabaseURL:      dbURL,
+		SalesMockURL:     salesURL,
+		ServiceMockURL:   serviceURL,
+		UpstreamTimeout:  upstreamTimeout,
+		RetryBaseDelay:   retryBaseDelay,
+		RequestDeadline:  requestDeadline,
+		BreakerThreshold: breakerThreshold,
 	}, nil
+}
+
+func parseDuration(envKey string, defaultMs int) time.Duration {
+	val := os.Getenv(envKey)
+	if val == "" {
+		return time.Duration(defaultMs) * time.Millisecond
+	}
+	ms, err := strconv.Atoi(val)
+	if err != nil {
+		return time.Duration(defaultMs) * time.Millisecond
+	}
+	return time.Duration(ms) * time.Millisecond
+}
+
+func parseUint32(envKey string, defaultVal uint32) uint32 {
+	val := os.Getenv(envKey)
+	if val == "" {
+		return defaultVal
+	}
+	n, err := strconv.ParseUint(val, 10, 32)
+	if err != nil {
+		return defaultVal
+	}
+	return uint32(n)
 }
